d7a5884b927c29ff750ae4e4312e4504837e7d2b
IamHPark <heesoo.p1023@gmail.com>
2022-05-02 23:35:46 +0000

    update data type

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -7,12 +7,11 @@ CREATE TABLE assignments
   chapter INTEGER,
   duration INTEGER
 );
-
 CREATE TABLE assignment_submissions
 (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration VARCHAR(32),
+  duration INTEGER,
   submission_date DATE
 );
